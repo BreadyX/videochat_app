4e093808b243366d7664e7822dbf9d4f73920f01
Alexandru Gabriel Bradatan <alex.bradatan85@yahoo.it>
2021-09-03 01:13:48 +0200

    frontend/components: Add Logo component
    Basic component that renders the svg logo with desired size. If passed
    'redirect' boolean, it redirect to `/` using vue-router.

---
+++ frontend/src/components/graphics/Logo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="logo">
+    <router-link to="/">
+      <img
+        class="logo__img"
+        :class="{ 'logo__img--shadow': shadow }"
+        src="@/assets/logo.svg"
+        :width="size"
+        :height="size"
+      />
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    redirect: {
+      type: Boolean,
+      default: () => false
+    },
+    size: {
+      type: Number,
+      default: () => 80
+    },
+    shadow: {
+      type: Boolean,
+      default: () => true
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+:root {
+  --logo-shadow-size: 5px;
+}
+</style>
+
+<style lang="scss" scoped>
+.logo__img {
+  display: block;
+}
+
+.logo__img--shadow {
+  filter: drop-shadow(0 0 var(--logo-shadow-size) var(--color-shadow));
+}
+</style>
